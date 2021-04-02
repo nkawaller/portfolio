3578f499dbc8c731d74a26a7502a6570d5338c12
Nathan Kawaller <kawaller@gmail.com>
2021-04-02 04:53:59 -0400

    underline animation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,22 +8,33 @@ const Nav: React.FC = () => {
           <a
             href="https://www.linkedin.com/in/nathan-kawaller-b035b01b6/"
             target="_blank"
+            className={navStyles.middle}
           >
             LinkedIn
           </a>
         </li>
         <li>
-          <a href="https://github.com/nkawaller" target="_blank">
+          <a
+            href="https://github.com/nkawaller"
+            target="_blank"
+            className={navStyles.middle}
+          >
             Github
           </a>
         </li>
         <li>
-          <a href="https://twitter.com" target="_blank">
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            className={navStyles.middle}
+          >
             Twitter
           </a>
         </li>
         <li>
-          <a href="mailto: kawaller@gmail.com">Email</a>
+          <a className={navStyles.middle} href="mailto: kawaller@gmail.com">
+            Email
+          </a>
         </li>
       </ul>
     </nav>
