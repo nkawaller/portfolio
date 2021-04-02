404022abfea39da6618ba5e28c1b401268af637a
Nathan Kawaller <kawaller@gmail.com>
2021-04-02 04:05:13 -0400

    body styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,19 @@ import headerStyles from "../styles/Header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <div>
+    <section className={headerStyles.bio}>
       <h1 className={headerStyles.title}>Nathan Kawaller</h1>
-
+      <br />
       <p className={headerStyles.description}>
-        Paragraph explaining who I am and what I do
+        First Paragraph explaining who I am and what I do
       </p>
-    </div>
+      <p className={headerStyles.description}>
+        Second Paragraph explaining who I am and what I do
+      </p>
+      <p className={headerStyles.description}>
+        Third Paragraph explaining who I am and what I do
+      </p>
+    </section>
   );
 };
 
